b64f74fe4f08159f52db1bc5146c60c5d391df75
jskbliy <67720215+jskbliy@users.noreply.github.com>
2022-11-04 23:22:43 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,13 +13,13 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: 'cb98b5f84682598252c1c07b86c89039',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '山东',
+  CITY: '寿光',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '晓玉',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-vVv58DHuGXufc8bt-K9Jrpcq_k',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
@@ -29,20 +29,20 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '1999', date: '03-22',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '陈铄', year: '1998', date: '11-30',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2019', date: '01-25',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2020-01-25' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
