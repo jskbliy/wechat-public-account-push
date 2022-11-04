05405414d1d721ef7f8fa3b1b06b4d4b4f54c603
jskbliy <67720215+jskbliy@users.noreply.github.com>
2022-11-05 00:07:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o-vVv50xRz05noRng7BWvjKghPI8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'KS6EgpC7UkZEoujvcWwzHEvp86QMXFevEN_0ZtOmGo8',
+      useTemplateId: 'Y5DFuzXekNGBcZUy2vUrBUwz-aQJ_FBEkJKVJRMREF0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
