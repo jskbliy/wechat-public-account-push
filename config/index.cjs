7a9123ef01efb3cbcc1b4544ce8c7e78cc298dfd
jskbliy <67720215+jskbliy@users.noreply.github.com>
2022-11-04 23:34:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '晓玉',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o-vVv58DHuGXufc8bt-K9Jrpcq_k',
+      id: 'o-vVv50xRz05noRng7BWvjKghPI8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'z99jfte--RTwTJDY3fqxvVUOHq_V35CQhfniSurN22I',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -57,7 +57,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o-vVv58DHuGXufc8bt-K9Jrpcq_k',
+      id: 'o-vVv50xRz05noRng7BWvjKghPI8',
     }
   ],
 
